221094753d784a54af7fa0e787f11353bfd02cf8
TemuujinTMJ <hades.tmj@gmail.com>
2021-12-20 08:25:26 +0000

    zz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ТУН УДАХГҮЙ</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Та манай апп ашиглан өөрт хамгийн ойр <br/> машиныг захиалан унаж, дуртай <br/> газраа орхиод явах боломжтой.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  );   
 }
 
 export default App;
